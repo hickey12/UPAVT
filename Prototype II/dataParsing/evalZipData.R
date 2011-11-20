6015743141f5896bef00b247382fadfd8159eb48
unknown <Trever Hickey@.(none)>
2011-11-19 19:16:24 -0800

    This is the new data parameters, including multiple states, metrics, and national zip code analysis.

--- Prototype II/dataParsing/evalZipData.R
+++ Prototype II/dataParsing/evalZipData.R
@@ -46,6 +46,7 @@ evalZipData <- function(state) {
 		#sorts the zips before processing.
 		zips <- sort(zips);
 
+		#Does the general summary information.
 		for(zip in zips)
 		{
 			inquiryCounter <- schoolTable$Zip[schoolTable$Zip == zip];
@@ -66,6 +67,8 @@ evalZipData <- function(state) {
 			enrolledCount <- c(enrolledCount, length(enrolledCounter));
 
 		}
+		
+		return (satVerbalHelperFunction(zips, schoolTable));
 
 		# Once a zips are complete, create a data frame to store these entries in a table
 		stateSummary <- (data.frame(Zipcodes = zips, Inquiries=inquiryCount, Applied=appliedCount, Accepted=acceptedCount, Enrolled=enrolledCount));	
@@ -73,4 +76,80 @@ evalZipData <- function(state) {
 		filename <- paste(path, paste("/summary", state, year, sep="_"), ".csv", sep="");;
 		write.csv(stateSummary, filename);
 	}
+}
+
+#Sorts the table into the bins to seperate the SAT verbal values.
+satVerbalHelperFunction <- function(zips, table)
+{	
+	#Holds the SAT seperators.
+	sats <- c(100, 200, 300, 400, 500, 600, 700, 800);
+	
+	satFrame <- data.frame(SATVerb100= c(), SATVerb200= c(), SATVerb300= c(), 
+										SATVerb400= c(), SATVerb500= c(), SATVerb600= c(), 
+										SATVerb700= c(), SATVerb800= c());
+	
+	#SAT subcounter
+	satSubCount <- c();
+	
+	#SAT counter
+	satCounter <- c();
+		
+	#Go through and makes all of the SAT bins
+	for(sat in sats)
+	{
+		#Clear the sat column.
+		satSubCount <- c();
+		
+		#Goes through all of the zipcodes
+		for(zip in zips)
+		{
+			#Go through bins of 100.
+			satCounter <- table$Zip[table$Zip == zip & 
+				table$SATVerbal <= sat & 
+				table$SATVerbal > sat-100];
+			
+			#Append the calculated SAT value to the zipcode.
+			satSubCount <- c(satSubCount, length(satCounter)); 
+		
+		}
+			
+
+	}
+
+	#Returns the SATbins.
+	#return (satSubCount);
+}
+
+#Sorts the table into bins to seperate the SAT math values.
+satMathHelperFunction <- function(zip, table)
+{
+	#SAT subcounter
+	satSubCount <- c();
+
+	#SAT counter
+	satCounter <- c();
+
+	#Holds the SAT seperators.
+	sats <- c(100, 200, 300, 400, 500, 600, 700, 800);
+	
+	#Go through and makes all of the SAT bins
+	for(sat in sats)
+	{
+		satCounter <- table$Zip[table$Zip == zip & 
+			table$SATMath <= sat & 
+			table$SATMath > sat-100]
+
+		satSubCount <- c(satSubCount, length(satCounter)); 
+	}
+
+	#Returns the SATbins for the zipcode.
+	return (satSubCount);
+}
+
+#Sorts the gpa values
+gpaHelperFunction <- function(table)
+{
+	#Holds the gpa seperators.
+	gpa <- c(1.0, 2.0, 3.0, 4.0);
+
 }
